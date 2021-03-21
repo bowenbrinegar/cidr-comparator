30896db013bf9fb46df357294c4803d017e0a576
Bowen Brinegar <bowenbrinegar@Bowens-MacBook-Pro.local>
2021-03-21 16:58:14 -0400

    replaced custom atoi with C99 equiv strtol

--- parseutils.h
+++ parseutils.h
@@ -12,26 +12,8 @@
 
 #endif //CIDR_COMPARISON_PARSEUTILS_H
 
-
-int atoiloc(const char* str){
-    int num = 0;
-    int i = 0;
-    bool isNegetive = false;
-    if(str[i] == '-'){
-        isNegetive = true;
-        i++;
-    }
-    while (str[i] && (str[i] >= '0' && str[i] <= '9')){
-        num = num * 10 + (str[i] - '0');
-        i++;
-    }
-    if(isNegetive) num = -1 * num;
-    return num;
-}
-
 int concatints(int a, int b)
 {
-
     char s1[20];
     char s2[20];
 
@@ -39,26 +21,9 @@ int concatints(int a, int b)
     sprintf(s2, "%d", b);
 
     strcat(s1, s2);
-
-    int c = atoiloc(s1);
-    return c;
-}
-
-
-int parse_ip(const char *ip) {
-    char parsed[15] = "";
-    int count = 0;
-    for (int i = 0; ip[i] != '\0'; i++) {
-        if (ip[i] != '.') {
-            parsed[count] = ip[i];
-            count++;
-        }
-    }
     char *ptr;
-
-    return (int) strtol(parsed, &ptr, 10);
+    return (int) strtol(s1, &ptr, 10);;
 }
-
 
 int first_digit(int digit) {
     int first = digit;
